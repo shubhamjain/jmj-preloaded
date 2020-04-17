d02e576f65a5a01bc336166f8cac63d68fedee59
Shubham Jain <shubham.jain.1@gmail.com>
2020-04-17 12:35:17 +0530

    orgin

---
+++ precache-manifest.353868afaa183952e796b070279c5bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b7d6a87962bbb15bbe4f7d39ec65b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a716029683f02d41307e",
+    "url": "/static/css/main.c26ec6dd.chunk.css"
+  },
+  {
+    "revision": "2380ebc26590373f20a3",
+    "url": "/static/js/2.12997a63.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/static/js/2.12997a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a716029683f02d41307e",
+    "url": "/static/js/main.0080ca6f.chunk.js"
+  },
+  {
+    "revision": "dc6c9145439f3ba64f0c",
+    "url": "/static/js/runtime-main.be5a99a7.js"
+  }
+]);
